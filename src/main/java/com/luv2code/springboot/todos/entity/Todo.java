8043d5b3d378f782d12c2f33aaac1b8290df4442
Saúl <147526498+saulito-tec@users.noreply.github.com>
2026-03-19 16:20:35 -0600

    initial commit

---
+++ src/main/java/com/luv2code/springboot/todos/entity/Todo.java
@@ -0,0 +1,69 @@
+package com.luv2code.springboot.todos.entity;
+
+import jakarta.persistence.*;
+
+@Table(name = "todos")
+@Entity
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private int priority;
+
+    @Column(nullable = false)
+    private boolean complete;
+
+    // private User owner;
+
+    // Default constructor (required by JPA)
+    public Todo() {}
+
+    public Todo(String title, String description, int priority, boolean complete) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.complete = complete;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public boolean isComplete() {
+        return complete;
+    }
+
+    public void setComplete(boolean complete) {
+        this.complete = complete;
+    }
+}

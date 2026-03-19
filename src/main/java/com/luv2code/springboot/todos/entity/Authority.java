e4aa006c9e48f48610c315216c205bd3185d4f94
Saúl <147526498+saulito-tec@users.noreply.github.com>
2026-03-19 16:38:17 -0600

    added user and authority classes

---
+++ src/main/java/com/luv2code/springboot/todos/entity/Authority.java
@@ -0,0 +1,22 @@
+package com.luv2code.springboot.todos.entity;
+
+import jakarta.persistence.Embeddable;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+
+@Embeddable
+public class Authority implements GrantedAuthority {
+
+    private String authority;
+
+    public Authority () {}
+
+    public Authority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public @Nullable String getAuthority() {
+        return null;
+    }
+}

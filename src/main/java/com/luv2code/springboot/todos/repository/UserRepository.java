400993d50df24f962f948c2f6c5e483738ac7ade
Saúl <147526498+saulito-tec@users.noreply.github.com>
2026-03-23 10:26:39 -0600

    added user repository and http security config filter

---
+++ src/main/java/com/luv2code/springboot/todos/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.luv2code.springboot.todos.repository;
+
+import com.luv2code.springboot.todos.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}

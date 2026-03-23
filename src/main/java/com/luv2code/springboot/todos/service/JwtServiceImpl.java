fadc2adbc1c4fc37bb54b1e26da65aa88d85ef14
Saúl <147526498+saulito-tec@users.noreply.github.com>
2026-03-23 09:35:59 -0600

    fixed jjwt version, created impl and interface

---
+++ src/main/java/com/luv2code/springboot/todos/service/JwtServiceImpl.java
@@ -0,0 +1,48 @@
+package com.luv2code.springboot.todos.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import javax.crypto.SecretKey;
+
+import java.util.Date;
+import java.util.Map;
+
+@Service
+public class JwtServiceImpl implements JwtService{
+
+    @Value("${spring.jwt.secret}")
+    private String SECRET_KEY;
+
+    @Value("${spring.jwt.expiration}")
+    private long JWT_EXPIRATION;
+
+    @Override
+    public String extractUsername(String token) {
+        return "";
+    }
+
+    @Override
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        return false;
+    }
+
+    @Override
+    public String generateToken(Map<String, Object> claims, UserDetails userDetails) {
+        return Jwts.builder()
+                .claims(claims)
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + JWT_EXPIRATION))
+                .signWith(getSigninKey(), Jwts.SIG.HS256)
+                .compact();
+    }
+
+    private SecretKey getSigninKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}

5e9a29920d58a48f518a4ce8ada29a895574dba9
Saúl <147526498+saulito-tec@users.noreply.github.com>
2026-03-26 10:58:30 -0600

    Added util for finding users

--- src/main/java/com/luv2code/springboot/todos/service/UserServiceImpl.java
+++ src/main/java/com/luv2code/springboot/todos/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.luv2code.springboot.todos.entity.Authority;
 import com.luv2code.springboot.todos.entity.User;
 import com.luv2code.springboot.todos.repository.UserRepository;
 import com.luv2code.springboot.todos.response.UserResponse;
+import com.luv2code.springboot.todos.util.FindAuthenticatedUser;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
@@ -15,20 +16,18 @@ import org.springframework.web.server.ResponseStatusException;
 @Service
 public class UserServiceImpl implements UserService{
     private final UserRepository userRepository;
+    private final FindAuthenticatedUser findAuthenticatedUser;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, FindAuthenticatedUser findAuthenticatedUser) {
         this.userRepository = userRepository;
+        this.findAuthenticatedUser = findAuthenticatedUser;
     }
 
     @Override
     @Transactional(readOnly = true)
     public UserResponse getUserInfo() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null || !authentication.isAuthenticated() || authentication.getPrincipal().equals("anonymousUser")){
-            throw new AccessDeniedException("Authentication required");
-        }
+        User user = findAuthenticatedUser.getAuthenticatedUser();
 
-        User user = (User) authentication.getPrincipal();
         return new UserResponse(
                 user.getId(),
                 user.getFirstName() + " " + user.getLastname(),
@@ -39,17 +38,11 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public void deleteUser() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null || !authentication.isAuthenticated() || authentication.getPrincipal().equals("anonymousUser")){
-            throw new AccessDeniedException("Authentication required");
-        }
-
-        User user = (User) authentication.getPrincipal();
+        User user = findAuthenticatedUser.getAuthenticatedUser();
 
         if(isLastAdmin(user)){
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Admin cannot delete itself");
         }
-
 
         userRepository.delete(user);
     }

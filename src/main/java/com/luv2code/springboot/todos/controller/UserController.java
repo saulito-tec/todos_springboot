2ffefae8049fee2deac66a1236c243d461c11fc7
Saúl <147526498+saulito-tec@users.noreply.github.com>
2026-03-27 10:20:09 -0600

    Added swagger tags to userController

--- src/main/java/com/luv2code/springboot/todos/controller/UserController.java
+++ src/main/java/com/luv2code/springboot/todos/controller/UserController.java
@@ -1,12 +1,13 @@
 package com.luv2code.springboot.todos.controller;
 
+import com.luv2code.springboot.todos.request.PasswordUpdateRequest;
 import com.luv2code.springboot.todos.response.UserResponse;
 import com.luv2code.springboot.todos.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "User REST API Endpoints", description = "Operations related to info about current user")
 @RestController
@@ -19,13 +20,24 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "User information", description = "Get current user info")
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/info")
     public UserResponse getUserInfo() {
         return userService.getUserInfo();
     }
 
+    @Operation(summary = "Delete user", description = "Delete current user account")
+    @ResponseStatus(HttpStatus.OK)
     @DeleteMapping
     public void deleteUser(){
         userService.deleteUser();
+    }
+
+    @Operation(summary = "Password update", description = "Change user password after verification")
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping("/password")
+    public void passwordUpdate(@Valid @RequestBody PasswordUpdateRequest passwordUpdateRequest)throws Exception{
+        userService.updatePassword(passwordUpdateRequest);
     }
 }

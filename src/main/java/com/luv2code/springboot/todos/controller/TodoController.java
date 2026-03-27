1ddaa0b22729ea98c0fec2a6ebd6e54d0c5cab59
Saúl <147526498+saulito-tec@users.noreply.github.com>
2026-03-27 10:50:02 -0600

    Added to-do controller

---
+++ src/main/java/com/luv2code/springboot/todos/controller/TodoController.java
@@ -0,0 +1,31 @@
+package com.luv2code.springboot.todos.controller;
+
+
+import com.luv2code.springboot.todos.request.TodoRequest;
+import com.luv2code.springboot.todos.response.TodoResponse;
+import com.luv2code.springboot.todos.service.TodoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "Todo REST API Endpoints", description = "Operations for managing user todos")
+@RestController
+@RequestMapping("/api/todos")
+public class TodoController {
+
+    private final TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @Operation(summary = "Create todo for user", description = "Create todo for the signed user")
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public TodoResponse createTodo(@Valid @RequestBody TodoRequest todoRequest){
+        return todoService.createTodo(todoRequest);
+    }
+
+}
